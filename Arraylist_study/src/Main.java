652a1a84549b2de92beba36b6a9ad213bd9d948f
yaozhangjun <3012374390@qq.com>
2024-03-14 12:01:46 +0800

    ,

--- Arraylist_study/src/Main.java
+++ Arraylist_study/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修复。
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello github!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
